cb9fad764f8ba495790997394fb903be7497f00d
Wan./Dita <103370604+Nagakaburos@users.noreply.github.com>
2022-09-08 15:48:08 -0300

    adicionando logica

--- app/src/main/java/com/estudocalccalculadorabasica/MainActivity.java
+++ app/src/main/java/com/estudocalccalculadorabasica/MainActivity.java
@@ -3,12 +3,22 @@ package com.estudocalccalculadorabasica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtN1;
+    private EditText txtN2;
+    private TextView txtResultadodaSoma;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtN1 = findViewById(R.id.txtN1);
+        txtN2 = findViewById(R.id.txtN2);
+        txtResultadodaSoma = findViewById(R.id.txtResultadodasSoma);
+
     }
 }
